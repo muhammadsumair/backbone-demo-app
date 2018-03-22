f48dca105c173a0fab7b0ea901101ae56ae4febc
msumair <msumair@nisum.com>
2018-03-22 13:04:58 +0500

    login web service added

--- server.js
+++ server.js
@@ -26,6 +26,26 @@ app.post('/login', function(req, res) {
     });
 });
 
+app.get('/users/:id', function(req, res) {
+    fs.readFile(path.join(__dirname + '/public/json/sample.json'), 'utf8', function (err, data) {
+        if (err) return res.status(500).send();
+        var sampleArray = JSON.parse(data);
+        var collection = sampleArray.filter(function(data) {
+            return data.id === req.params.id;
+        });
+        if(collection.length) return res.send(collection);
+        return res.status(404).send({message: 'Data not found'});
+    });
+});
+
+app.get('/users', function(req, res) {
+    fs.readFile(path.join(__dirname + '/public/json/sample.json'), 'utf8', function (err, data) {
+        if (err) return res.status(500).send();
+        var sampleArray = JSON.parse(data);
+        res.send(sampleArray);
+    });
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
