9c073a007813daf13d220cd0c0127f4bd61f1e37
msumair <msumair@nisum.com>
2018-03-22 12:51:27 +0500

    login web service added

--- server.js
+++ server.js
@@ -1,11 +1,31 @@
 var express = require('express');
 var path = require('path');
+var fs = require('fs');
+var bodyParser = require('body-parser');
+
 const port = 3000;
 
 var app = express();
 
 //setting middleware
 app.use(express.static(__dirname + '/public')); //Serves resources from public folder
+app.use(bodyParser.json());
+
+app.post('/login', function(req, res) {
+    fs.readFile(path.join(__dirname + '/public/json/credentials.json'), 'utf8', function (err, data) {
+        if (err) return res.status(500).send();
+        var users = JSON.parse(data);
+        for(var i = 0; i < users.length; i++) {
+            if(users[i].email === req.body.email && users[i].password === req.body..password) {
+                return res.status(200).send({
+                    user: users[i]
+                })
+            }
+        }
+        return res.status(404).send({message: 'User not found'});
+    });
+});
+
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/public/index.html'));
 });
